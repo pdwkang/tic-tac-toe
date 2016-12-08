94264172b3c9fda895613833104a50b4f55bb839
Paul Kang <paulkang@Pauls-MacBook-Air-2.local>
2016-12-08 10:57:29 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// var container = document.getElementsByClassName('container');
+// console.log(container);
+// container[0].innerHTML = "hello, world"
+
+// 1. set up board
+// 2. user should be able to click a box and a mark shows up
+// 	put an onclick in the first square
+// 	when user clicks, call function that puts an x in the box.
+// milestone
+// 3. 
+
+function markSquare(square){
+	console.log(square.id);
+}
